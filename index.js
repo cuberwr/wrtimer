7e0180b1805e6324f3c110b63612bb9c942559b1
cuberwr <wangrun.vip@outlook.com>
2019-06-16 06:54:20 +0800

    Update index.js

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ server = express();
 
 server.use(express.static('front'));
 
-server.get('/src', (req, res) => {
+/*server.get('/src', (req, res) => {
     agent.get('http://localhost:2014/')
     res.send('###');
-});
+});*/
 
 server.listen(8080, () => {console.log("listing 8080");});
